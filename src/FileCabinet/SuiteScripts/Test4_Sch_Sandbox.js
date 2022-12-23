198423b155ef660a1113fd6b5aa75dfbe6e55a51
Lalitha <lalliannam2107@gmail.com>
2022-12-23 21:14:02 +0530

    Dev to Sandbox third Commit

---
+++ src/FileCabinet/SuiteScripts/Test4_Sch_Sandbox.js
@@ -0,0 +1,25 @@
+/**
+ * @NApiVersion 2.1
+ * @NScriptType ScheduledScript
+ */
+define([],
+    
+    () => {
+
+        /**
+         * Defines the Scheduled script trigger point.
+         * @param {Object} scriptContext
+         * @param {string} scriptContext.type - Script execution context. Use values from the scriptContext.InvocationType enum.
+         * @since 2015.2
+         */
+        const execute = (scriptContext) => {
+            try{
+               log.debug("Entered in schedule script")
+                }catch(e){
+                    log.debug(e.name,e.message)
+                }
+        }
+
+        return {execute}
+
+    });
